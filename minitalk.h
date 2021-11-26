d86869ecf66cedddc61980531a18825a11d17650
GoDroid42 <leetpwnsroot@gmail.com>
2021-11-26 17:00:34 +0100

    Finished

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsabir <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 12:39:42 by hsabir            #+#    #+#             */
+/*   Updated: 2021/11/26 14:49:06 by hsabir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./ft_printf/libft/libft.h"
+# include "./ft_printf/ft_printf.h"
+# include <stdint.h>
+# include <signal.h>
+# include <stdbool.h>
+# include <stdlib.h>
+
+#endif
---
+++ server.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsabir <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 12:42:15 by hsabir            #+#    #+#             */
+/*   Updated: 2021/11/26 14:53:17 by hsabir           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	convert_msg(int signum)
+{
+	static int	power;
+	static int	byte;
+
+	if (signum == SIGUSR1)
+		byte += 1 << (7 - power);
+	power++;
+	if (power == 8)
+	{
+		ft_printf("%c", byte);
+		power = 0;
+		byte = 0;
+	}
+}
+
+int	main()
+{
+	//struct sigaction	usr1;
+	//struct sigaction	usr2;
+	pid_t	pid;
+	
+	pid = getpid();
+	ft_printf("PID: %d\n", pid);
+	signal(SIGUSR1, convert_msg);
+	signal(SIGUSR2, convert_msg);
+	while (1)
+		pause();
+}
